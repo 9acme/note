adabc9847caaa536b33770b2c00655d39ed76d30
fengqing <3451785715@qq.com>
2021-04-16 17:03:31 +0800

    修改base为https://cdn.jsdelivr.net/gh/annarheimur/note/dist/

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // base: 'https://cdn.jsdelivr.net/gh/Annarheimur/note@main/dist/',
+  base: 'https://cdn.jsdelivr.net/gh/annarheimur/note/dist/',
   dest: './dist',
   title: '学习笔记',
   themeConfig: {
@@ -11,16 +11,5 @@ module.exports = {
         children: ['webpack/1.md', 'webpack/2.md', 'webpack/3.md']
       }
     ]
-  },
-  configureWebpack: () => {
-    const NODE_ENV = process.env.NODE_ENV;
-    //判断是否是生产环境
-    if (NODE_ENV === 'production') {
-      return {
-        output: {
-          publicPath: 'https://cdn.jsdelivr.net/gh/Annarheimur/note@main/dist/'
-        }
-      };
-    }
   }
 };
