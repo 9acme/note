e4df93087a7f7e3fb5388f7b88d3fad46b50c0bf
fengqingyang <3451785715@qq.com>
2021-11-23 13:40:10 +0800

    添加mysql与jdbc的笔记

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -37,20 +37,6 @@ module.exports = {
         path: 'typescript/typescript.md'
       },
       {
-        title: 'java',
-        collapsable: true,
-        sidebarDepth: 2,
-        children: [
-          { title: '概述', path: 'javaSE/1_概述.md' },
-          { title: '变量', path: 'javaSE/2_变量标识符保留字.md' },
-          { title: '运算符', path: 'javaSE/3_运算符.md' },
-          { title: '流程控制', path: 'javaSE/4_流程控制.md' },
-          { title: '数组', path: 'javaSE/5_数组.md' },
-          { title: '异常', path: 'javaSE/9_异常.md' },
-          { title: '多线程', path: 'javaSE/10_多线程.md' }
-        ]
-      },
-      {
         title: '自定义工具库',
         collapsable: true,
         children: [
@@ -75,6 +61,52 @@ module.exports = {
           'JS工具库/03_发布到npm中央仓库',
           'JS工具库/04_使用自定义工具包'
         ]
+      },
+      {
+        title: 'java',
+        collapsable: true,
+        sidebarDepth: 2,
+        children: [
+          { title: '概述', path: 'javaSE/1_概述.md' },
+          { title: '变量', path: 'javaSE/2_变量标识符保留字.md' },
+          { title: '运算符', path: 'javaSE/3_运算符.md' },
+          { title: '流程控制', path: 'javaSE/4_流程控制.md' },
+          { title: '数组', path: 'javaSE/5_数组.md' },
+          { title: '异常', path: 'javaSE/9_异常.md' },
+          { title: '多线程', path: 'javaSE/10_多线程.md' }
+        ]
+      },
+      {
+        title: 'mysql',
+        collapsable: true,
+        sidebarDepth: 2,
+        children: [
+          { title: '总纲', path: 'mysql/第00章_写在前面.md' },
+          { title: '概述', path: 'mysql/第01章_数据库概述.md' },
+          { title: '环境搭建', path: 'mysql/第02章_MySQL环境搭建.md' },
+          { title: '基本的SELECT语句', path: 'mysql/第03章_基本的SELECT语句.md' },
+          { title: '运算符', path: 'mysql/第04章_运算符.md' },
+          { title: '排序与分页', path: 'mysql/第05章_排序与分页.md' },
+          { title: '多表查询', path: 'mysql/第06章_多表查询.md' },
+          { title: '单行函数', path: 'mysql/第07章_单行函数.md' },
+          { title: '聚合函数', path: 'mysql/第08章_聚合函数.md' },
+          { title: '子查询', path: 'mysql/第09章_子查询.md' },
+          { title: '创建和管理表', path: 'mysql/第10章_创建和管理表.md' },
+          { title: '数据处理之增删改', path: 'mysql/第11章_数据处理之增删改.md' },
+          { title: '数据类型精讲', path: 'mysql/第12章_MySQL数据类型精讲.md' },
+          { title: '约束', path: 'mysql/第13章_约束.md' },
+          { title: '视图', path: 'mysql/第14章_视图.md' },
+          { title: '存储过程与函数', path: 'mysql/第15章_存储过程与函数.md' },
+          { title: '变量、流程控制与游标', path: 'mysql/第16章_变量、流程控制与游标.md' },
+          { title: '触发器', path: 'mysql/第17章_触发器.md' },
+          { title: 'MySQL8其它新特性', path: 'mysql/第18章_MySQL8其它新特性.md' }
+        ]
+      },
+      {
+        title: 'JDBC',
+        collapsable: true,
+        sidebarDepth: 2,
+        path: 'jdbc/jdbc.md'
       }
     ],
     // 默认值是 true 。设置为 false 来禁用所有页面的 下一篇 链接
